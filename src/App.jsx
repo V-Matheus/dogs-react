1f9c9278f5b7d8fe1b56a3b708a82bd43c736008
V-Matheus <victormatheus507@gmail.com>
2023-11-08 16:15:31 -0300

    primeiros componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Api from './api/Api';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Home from './Components/Home'
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };

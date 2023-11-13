0af90ce9f859b8a4c9bab2c5e46a69b779877fdf
V-Matheus <victormatheus507@gmail.com>
2023-11-13 16:30:21 -0300

    navigate

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = React.createContext();
 
@@ -8,6 +9,7 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  const navigate = useNavigate()
 
   React.useEffect(() => {
     async function autoLogin() {
@@ -48,6 +50,7 @@ export const UserStorage = ({ children }) => {
       const { token } = await tokenRes.json();
       window.localStorage.setItem('token', token);
       await getUser(token);
+      navigate('./conta')
     } catch (err) {
       setError(err.message)
       setLogin(false)
@@ -62,6 +65,7 @@ export const UserStorage = ({ children }) => {
     setLoading(false);
     setLogin(false);
     window.localStorage.removeItem('token');
+    navigate('./login')
   }
 
   return (

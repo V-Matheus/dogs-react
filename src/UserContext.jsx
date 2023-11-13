f3e1ad670d8e1a9d135eb9ebf60825c796941a31
V-Matheus <victormatheus507@gmail.com>
2023-11-13 16:25:45 -0300

    mensage error login

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -19,9 +19,9 @@ export const UserStorage = ({ children }) => {
           const { url, options } = TOKEN_VALIDATE_POST(token);
           const response = await fetch(url, options);
           if (!response.ok) throw new Error('Token inválido');
-          await getUser(token)
+          await getUser(token);
         } catch (err) {
-          userLogout()
+          userLogout();
         } finally {
           setLoading(false);
         }
@@ -39,24 +39,33 @@ export const UserStorage = ({ children }) => {
   }
 
   async function userLogin(username, password) {
-    const { url, options } = TOKEN_POST({ username, password });
-    const tokenRes = await fetch(url, options);
-    const { token } = await tokenRes.json();
-    window.localStorage.setItem('token', token);
-    getUser(token);
+    try {
+      setError(null)
+      setLoading(true)
+      const { url, options } = TOKEN_POST({ username, password });
+      const tokenRes = await fetch(url, options);
+      if(!tokenRes.ok) throw new Error(`Error: Usuário inválido`)
+      const { token } = await tokenRes.json();
+      window.localStorage.setItem('token', token);
+      await getUser(token);
+    } catch (err) {
+      setError(err.message)
+      setLogin(false)
+    } finally {
+      setLoading(false)
+    }
   }
 
   async function userLogout() {
-      setData(null)
-      setError(null)
-      setLoading(false)
-      setLogin(false)
-      window.localStorage.removeItem('token')
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
+    window.localStorage.removeItem('token');
   }
 
-
   return (
-    <UserContext.Provider value={{ userLogin, userLogout, data }}>
+    <UserContext.Provider value={{ userLogin, userLogout, data, error, loading, login }}>
       {children}
     </UserContext.Provider>
   );

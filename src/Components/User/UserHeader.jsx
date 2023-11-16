908c9ec9f53e2558cf2f9bdccb1c63f0be01a40e
V-Matheus <victormatheus507@gmail.com>
2023-11-16 16:58:26 -0300

    user styles

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import UserHeaderNav from './UserHeaderNav'
+import styles from './UserHeader.module.css'
 
 const UserHeader = () => {
   return (
-    <header>
-      <h1 className='title'>Título</h1>
+    <header className={styles.header}>
+      <h1 className='title'>Título</h1> 
       <UserHeaderNav />
     </header>
   )

b033d6a71abcf46aa0749cd51ce616e98df87057
V-Matheus <victormatheus507@gmail.com>
2023-11-16 16:40:51 -0300

    user icons

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import {UserContext} from '../../UserContext'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
 
 const UserHeaderNav = () => {
-
-  const {userLogout} = React.useContext(UserContext)
+  const { userLogout } = React.useContext(UserContext);
   return (
     <nav>
-      <NavLink to='/conta'>Minhas Fotos</NavLink>
-      <NavLink to='/conta/estatisticas'>Estatísticas</NavLink>
-      <NavLink to='/conta/postar'>Adicionar Foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+      <NavLink to="/conta"><MinhasFotos/></NavLink>
+      <NavLink to="/conta/estatisticas"><Estatisticas /></NavLink>
+      <NavLink to="/conta/postar"><AdicionarFoto/></NavLink>
+      <button onClick={userLogout}><Sair/></button>
     </nav>
-  )
-}
+  );
+};
 
-export default UserHeaderNav
+export default UserHeaderNav;

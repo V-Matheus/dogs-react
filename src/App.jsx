46815894ca3ef8a76a073627d2b055683561be86
V-Matheus <victormatheus507@gmail.com>
2023-11-16 15:50:03 -0300

    rout conta

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import User from './Components/User/User';
 
 const App = () => {
   return (
@@ -15,7 +16,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>

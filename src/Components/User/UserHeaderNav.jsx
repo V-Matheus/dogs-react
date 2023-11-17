9f959904962e2ea14027ebb43d9a1d72ec331fa5
V-Matheus <victormatheus507@gmail.com>
2023-11-17 16:55:58 -0300

    media query

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,12 +6,14 @@ import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
+import { useMedia } from '../../Hooks/useMedia';
 
 const UserHeaderNav = () => {
-  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
-  const navigate = useNavigate();  
-  
+  const navigate = useNavigate();
+  const mobile = useMedia('(max-width: 40rem)');
+  console.log(mobile);
+
   function handleLogout() {
     userLogout();
     navigate('/login');

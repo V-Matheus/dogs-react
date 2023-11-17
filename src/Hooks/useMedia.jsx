446f228d79b32956163b795e6f3f0d3920ad39b2
V-Matheus <victormatheus507@gmail.com>
2023-11-17 17:27:34 -0300

    ativar menu mobile assim que entra

--- src/Hooks/useMedia.jsx
+++ src/Hooks/useMedia.jsx
@@ -8,6 +8,7 @@ export const useMedia = (media) => {
       const { matches } = window.matchMedia(media);
       setMatch(matches)
     }
+    changeMatch()
     window.addEventListener('resize', changeMatch)
     return () => {
       window.removeEventListener('resize', changeMatch)

0b69fe8dc97a6f648a35004f0d055a54fae7152f
V-Matheus <victormatheus507@gmail.com>
2023-11-30 16:53:39 -0300

    puxando as fotos da api

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import FeedPhotosItem from './FeedPhotosItem'
-import useFetch from '../../Hooks/useFetch'
-import { func } from 'prop-types'
+import React from 'react';
+import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 
 const FeedPhotos = () => {
-
-  const [data, loading, error, request] = useFetch()
+  const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const {url, options} =
-      request(url, options)
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { response, json } = await request(url, options);
+      console.log(json);
     }
-    fetchPhotos()
-  }, [])
-  return (
-    <div>
-      <FeedPhotosItem />
-    </div>
-  )
-}
+    fetchPhotos();
+  }, [request]);
 
-export default FeedPhotos
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </div>
+    );
+  else return null;
+};
+
+export default FeedPhotos;

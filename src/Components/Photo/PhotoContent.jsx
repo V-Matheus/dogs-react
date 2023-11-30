cabfaae3e16457bbf1a1477a9bc962937b2511f6
V-Matheus <victormatheus507@gmail.com>
2023-11-30 17:46:33 -0300

    exibir detalhes do modal

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from './PhotoContent.module.css';
+import { Link } from 'react-router-dom';
 
-const PhotoContent = ({data}) => {
+const PhotoContent = ({ data }) => {
+  const { photo, comments } = data;
   return (
-    <div>
-      
+    <div className={styles.photos}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visulizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className='title'>
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} Kg</li>
+            <li>{photo.idade} anos</li>
+          </ul>
+        </div>
+      </div>
+ 
     </div>
-  )
-}
+  );
+};
 
-export default PhotoContent
+export default PhotoContent;

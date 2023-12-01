e0808b6a2edf383ba077beac9adc5a8d55f5bd3f
Victor Matheus <victormatheus507@gmail.com>
2023-12-01 10:31:15 -0300

    PhotoContent style

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -9,23 +9,23 @@ const PhotoContent = ({ data }) => {
     <div className={styles.photo}>
       <div className={styles.img}>
         <img src={photo.src} alt={photo.title} />
-        <div className={styles.details}>
-          <div>
-            <p>
-              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-              <span className={styles.visualizacoes}>{photo.acessos}</span>
-            </p>
-            <h1 className='title'>
-              <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
-            </h1>
-            <ul className={styles.attributes}>
-              <li>{photo.peso} Kg</li>
-              <li>{photo.idade} Anos</li>
-            </ul>
-          </div>
-        </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <div className={styles.details}>
+        <div>
+          <p>
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
+          </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} Kg</li>
+            <li>{photo.idade} Anos</li>
+          </ul>
+        </div>
+        <PhotoComments id={photo.id} comments={comments} />
+      </div>
     </div>
   );
 };

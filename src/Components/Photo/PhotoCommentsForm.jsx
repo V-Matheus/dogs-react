8e589d85578445e5f53b649fbd5f177976cdf1c6
Victor Matheus <victormatheus507@gmail.com>
2023-12-03 16:58:07 -0300

    mudando o nome do arquivo para api

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
-import { COMMENT_POST } from '../../Api';
+import { COMMENT_POST } from '../../api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
-import styles from './PhotoCommentsForm.module.css'
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments, single }) => {
   const [comment, setComment] = useState('');
-  const {request, error} = useFetch()
+  const { request, error } = useFetch();
 
   async function handleSubmit(event) {
-    event.preventDefault()
-    const {url,options} = COMMENT_POST(id, {comment})
-    const {response, json} = await request(url,options)
-    if(response.ok) {
-      setComment('')
-      setComments((comments) => [...comments, json])
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
     }
   }
   return (
-    <form className={`${styles.form} ${single ? styles.single : ''}`} onSubmit={handleSubmit}>
+    <form
+      className={`${styles.form} ${single ? styles.single : ''}`}
+      onSubmit={handleSubmit}
+    >
       <textarea
         className={styles.textarea}
         id="commnent"
         name="commnent"
-        placeholder='Comente...'
+        placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />

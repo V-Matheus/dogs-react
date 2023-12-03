f17efe66e54824345e1a2fbbf3aeecea46aae179
Victor Matheus <victormatheus507@gmail.com>
2023-12-03 14:44:25 -0300

    PASSWORD_LOST

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
 
 const LoginPasswordLost = () => {
+
+  const email = useForm()
+  const {data,loading, error, request} = useFetch()
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    request(url,options)
+  }
   return (
-    <div>LoginPasswordLost</div>
+    <section>
+      <h1 className='title'>Perdeu a senha ?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label='Email / Usuário' type='text' name='email' {...email}/>
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
   )
 }
 

549409b1f8a38b0a088b055657aadfa03580adf7
V-Matheus <victormatheus507@gmail.com>
2023-12-15 18:04:19 -0300

    configureStore

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './App.jsx';
+import {Provider} from 'react-redux'
+import store from './store/configureStore'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);

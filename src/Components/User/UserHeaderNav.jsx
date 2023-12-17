a50062c8f6c9f7d6eb82f3df4e23a10a8550a89c
Victor Matheus <victormatheus507@gmail.com>
2023-12-17 17:34:10 -0300

    userAutoLogin

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-import { UserContext } from '../../UserContext';
 import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
 import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
 import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
 import { ReactComponent as Sair } from '../../Assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
 import { useMedia } from '../../Hooks/useMedia';
+import { useDispatch } from 'react-redux';
+import { userLogout } from '../../store/user';  
 
 const UserHeaderNav = () => {
-  const { userLogout } = React.useContext(UserContext);
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
   const mobile = useMedia('(max-width: 40rem)');
   const [mobileMenu, setMobileMenu] = React.useState(false);
-
-  function handleLogout() {
-    userLogout();
-    navigate('/login');
-  }
 
   const {pathname} = useLocation()
   React.useEffect(() => {
@@ -53,7 +48,7 @@ const UserHeaderNav = () => {
           <AdicionarFoto />
           {mobile && 'Adicionar Foto'}
         </NavLink>
-        <button onClick={handleLogout}>
+        <button onClick={() => dispatch(userLogout())}>
           <Sair />
           {mobile && 'Sair'}
         </button>
